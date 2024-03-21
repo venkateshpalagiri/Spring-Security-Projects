4c80830faaefa1b7296542def3f45545327621f0
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-03-21 18:42:00 -0400

    Authentication Controller created

---
+++ springJwt/src/main/java/com/venkatesh/springJwt/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.venkatesh.springJwt.controller;
+
+import com.venkatesh.springJwt.model.AuthenticationResponse;
+import com.venkatesh.springJwt.model.User;
+import com.venkatesh.springJwt.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+    public AuthenticationController(AuthenticationService authenticationService){
+        this.authenticationService=authenticationService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody User request){
+        return ResponseEntity.ok(authenticationService.register(request));
+    }
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody User request){
+        return ResponseEntity.ok(authenticationService.authenticate(request));
+
+    }
+}
